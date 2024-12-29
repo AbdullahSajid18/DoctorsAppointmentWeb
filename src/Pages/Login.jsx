a4ebc2aa72188866c3a30ec813fdaa179832309d
AbdullahSajid18 <abdullahsajid1098@gmail.com>
2024-12-29 23:31:33 +0500

    Updated

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 
 const Login = () => {
-  return (
-    <div>
-        
-    </div>
-  )
-}
+  const [state, setState] = useState("Sign Up");
 
-export default Login
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className="min-h-[80vh] flex items-center ">
+      <div className="flex flex-col items-start gap-3 p-8 m-auto min-w-[340px] sm:min-w-96 border rounded-xl text-zinc-600 text-sm shadow-lg  ">
+        <p className="text-2xl font-semibold">
+          {state === "Sign Up" ? "Create Account" : "Log In"}
+        </p>
+        <p>
+          Please {state === "Sign Up" ? "sign up" : "log in"} to book an
+          appointment
+        </p>
+        {
+          state === "Sign Up" && <div className="w-full">
+          <p>Full Name</p>
+          <input
+            className="w-full p-2 mt-1 border rounded border-zinc-300"
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        }
+        
+        <div className="w-full">
+          <p>Email</p>
+          <input
+            className="w-full p-2 mt-1 border rounded border-zinc-300"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="w-full">
+          <p>Password</p>
+          <input
+            className="w-full p-2 mt-1 border rounded border-zinc-300"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button className='w-full py-2 text-base text-white rounded-md bg-primary' >{state === "Sign Up" ? "Create Account" : "Login"}</button>
+
+        {
+          state === "Sign Up"
+          ? <p>Already have an account? <span onClick={(()=> setState('Login'))} className='underline cursor-pointer text-primary'>Login Here</span></p>
+          : <p>Create a new account? <span onClick={(()=> setState('Sign Up'))} className='underline cursor-pointer text-primary'>Click here</span></p>
+        }
+      </div>
+    </form>
+  );
+};
+
+export default Login;
--- src/Pages/MyProfile.jsx
+++ src/Pages/MyProfile.jsx
@@ -1,11 +1,96 @@
-import React from 'react'
+import { useState } from 'react'
+import { assets } from '../assets/assets_client/assets';
 
 const MyProfile = () => {
+
+  const [userData, setUserData] = useState({
+    name: 'Edward Vincent',
+    image: assets.profile_pic,
+    email: 'richardjames@gmail.com',
+    phone: '+1 123 456 7890',
+    address:{
+      line1: '57th Cross Richmond',
+      line2: 'Circle, Church Road, London'
+    } ,
+    gender: 'Male',
+    dob: '28/04/2005'
+  });
+
+  const [isEditable, setIsEditable] = useState(false);
+
   return (
-    <div>
+    <div className='flex flex-col max-w-lg gap-2 text-sm'>
         
+      <img className='rounded w-36' src={userData.image} alt="" />
+
+      {
+        isEditable
+        ? <input className='mt-4 text-3xl font-medium bg-gray-50 max-w-60' type='text' onChange={e => setUserData(prev => ({...prev, name: e.target.value}))} value={userData.name} />
+        : <p className='mt-4 text-3xl fonr-medium text-neutral-800'>{userData.name} </p>
+      }
+
+      <hr className='bg-zinc-400 h-[1px] border-none ' />
+      <div>
+        <p className='mt-3 underline text-neutral-500'>CONTACT INFORMATION</p>
+        <div className='grid grid-cols-[1fr_3fr] gap-y-2.5 mt-3 text-neutral-700 '>
+          <p className='font-medium '>Email Id:</p>
+          <p className='text-blue-500'>{userData.email}</p>
+          <p className='font-medium '>Phone Number:</p>
+          {
+            isEditable
+            ? <input className='bg-gray-100 max-w-52' type='text' onChange={e => setUserData(prev => ({...prev, phone: e.target.value}))} value={userData.phone} />
+            : <p className='text-blue-400'>{userData.phone} </p>
+          }
+
+          <p className='font-medium'>Address:</p>
+          {
+            isEditable
+            ? <p>
+                <input className='bg-gray-50' onChange={(e) => setUserData(prev => ({...prev, address: {...prev.address, line1: e.target.value }}))} value={userData.address.line1} type="text" />
+                <br />
+                <input className='bg-gray-50' onChange={(e) => setUserData(prev => ({...prev, address: {...prev.address, line2: e.target.value }}))} value={userData.address.line2} type="text" />
+              </p>
+            : <p className='text-gray-500'>
+                {userData.address.line1}
+                <br />
+                {userData.address.line2}
+            </p>
+          }
+        </div>
+      </div>
+      <div>
+        <p className='mt-3 underline text-neutral-500'>BASIC INFORMATION</p>
+        <div>
+          <p>Gender:</p>
+          {
+            isEditable
+           ? <select onChange={e => setUserData(prev => ({...prev, gender: e.target.value}))} value= {userData.gender} >
+           <option value="Male">Male</option>
+           <option value="Female">Female</option>
+           </select>
+           : <p>{userData.gender} </p>
+          }
+          <p>Date of Birth:</p>
+          {
+            isEditable 
+            ? <input type='date' onChange={e => setUserData(prev => ({...prev, dob: e.target.value}))} value={userData.dob} />
+            : <p>{userData.dob}</p>
+          }
+        </div>
+      </div>
+
+      <div>
+        {
+          isEditable
+         ? <button onClick={() => setIsEditable(false)}>Save Information</button>
+         : <button onClick={() => setIsEditable(true)}>Edit</button> 
+        }
+      </div>
+
+
+
     </div>
   )
 }
 
-export default MyProfile
+export default MyProfile;
